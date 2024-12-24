63f783404000bff66469805cdd5aa5d7bd7c5934
isee <stpark0509@gmail.com>
2024-12-24 17:17:58 +0900

    first

---
+++ kpro_test/src/main/java/kr/co/emfo/kpro_test/KproTestApplication.java
@@ -0,0 +1,13 @@
+package kr.co.emfo.kpro_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KproTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KproTestApplication.class, args);
+	}
+
+}
