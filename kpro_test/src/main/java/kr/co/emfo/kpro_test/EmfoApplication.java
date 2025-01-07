2332fcf3da835db3774c5c8f98da56fb3813faa1
isee <stpark0509@gmail.com>
2025-01-07 17:26:40 +0900

    - 웹서버 NONE 설정 수정

--- kpro_test/src/main/java/kr/co/emfo/kpro_test/EmfoApplication.java
+++ kpro_test/src/main/java/kr/co/emfo/kpro_test/EmfoApplication.java
@@ -1,9 +1,12 @@
 package kr.co.emfo.kpro_test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.swing.*;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -11,7 +14,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class EmfoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmfoApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(EmfoApplication.class);
+		springApplication.setWebApplicationType(WebApplicationType.NONE);
+		springApplication.run(args);
+
+		/*SpringApplication.run(EmfoApplication.class, args);*/
 	}
 
 }
